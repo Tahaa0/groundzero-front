f019b2402284fd7265bc9d5130e423664a9021a6
ProgSaleh <progsaleh@hotmail.com>
2023-09-18 21:44:02 +0300

    adding form control placeholders in Arabic and a how-to-fill paragraph to the form pages

--- src/components/AddMissingPerson.js
+++ src/components/AddMissingPerson.js
@@ -7,147 +7,200 @@ import { directus } from '../services/directus';
 import { createItem } from '@directus/sdk';
 
 const AddMissingPerson = () => {
+  const [name, setName] = useState('');
+  const [villageName, setVillageName] = useState('');
+  const [location, setLocation] = useState('');
+  const [age, setAge] = useState('');
+  const [sex, setSex] = useState('male');
+  const [phone, setPhone] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [info, setInfo] = useState('');
 
-    const [name, setName] = useState('');
-    const [villageName, setVillageName] = useState('');
-    const [location, setLocation] = useState('');
-    const [age, setAge] = useState('');
-    const [sex, setSex] = useState('male');
-    const [phone, setPhone] = useState('');
-    const [whatsapp, setWhatsapp] = useState('');
-    const [info, setInfo] = useState('');
+  const cleanForm = () => {
+    setName('');
+    setVillageName('');
+    setLocation('');
+    setAge('');
+    setSex('');
+    setPhone('');
+    setWhatsapp('');
+    setInfo('');
+  };
 
-    const cleanForm = () => {
-        setName('');
-        setVillageName('');
-        setLocation('');
-        setAge('');
-        setSex('');
-        setPhone('');
-        setWhatsapp('');
-        setInfo('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (phone + whatsapp === '') {
+      window.notifyRed('Besoin de spécifier numéro tel ou whatsapp');
+    } else {
+      try {
+        const missing = await directus.request(
+          createItem('missing_persons', {
+            name: name,
+            villageName: villageName,
+            googlemaplink: location,
+            age: Number(age),
+            gender: sex,
+            phone: phone,
+            whatsapp: whatsapp,
+            info: info,
+          })
+        );
+        window.notify('Personne disparue ajoutée avec succès.');
+        cleanForm();
+      } catch (error) {
+        window.notifyRed("Erreur lors de l'ajout de la personne disparue.");
+      }
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (phone + whatsapp === "") {
-            window.notifyRed('Besoin de spécifier numéro tel ou whatsapp')
-        }
-        else {
-            try {
-                const missing = await directus.request(createItem('missing_persons', {
-                    name: name,
-                    villageName: villageName,
-                    googlemaplink: location,
-                    age: Number(age),
-                    gender: sex,
-                    phone: phone,
-                    whatsapp: whatsapp,
-                    info: info
-                }));
-                window.notify('Personne disparue ajoutée avec succès.');
-                cleanForm();
-            } catch (error) {
-                window.notifyRed('Erreur lors de l\'ajout de la personne disparue.');
-            }
-        }
-    }
+  return (
+    <>
+      <Container>
+        <form className="add-missing-person mt-40">
+          <Row>
+            <h1 className="form-tab-header">Lancer un avis de recherche</h1>
+            <div className="d-flex justify-content-center text-center mb-4">
+              <article>
+                <p className="d-inline">
+                  Comment ça marche?
+                  <br />
+                  Aller sur google map, poser un pin 📍, cliquer sur partager et
+                  copié le lien. Coller le lien dans "lien google map". Les
+                  posts sans numéro de contacts (numéro de télephone ou what's
+                  app) ne seront pas
+                </p>
+                <br />
+                <p className="d-inline font-cairo">
+                  كيف تعمل؟
+                  <br />
+                  اذهب إلى خريطة جوجل، ضع دبوس📍، ثم اضغط على مشاركة وانسخ
+                  الرابط. الصق الرابط في "رابط خريطة جوجل". لن يتم الموافقة على
+                  المنشورات التي لا تحتوي على رقم اتصال (رقم الهاتف او الواتساب)
+                </p>
+              </article>
+            </div>
+            <Col md={6}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Nom de la personne (obligatoire) :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Nom de la personne  (obligatoire) اسم الشخص (مطلوب)"
+                  />
+                </div>
+              </div>
+            </Col>
 
-    return (
-        <>
-        <Container>
-            <form className="add-missing-person mt-40">
-            <Row>
-            <h1 className='form-tab-header'>Lancer un avis de recherche</h1>
-                <Col md={6}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Nom de la personne (obligatoire) :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' value={name} onChange={e => setName(e.target.value)} placeholder='Nom de la personne  (obligatoire)' />
-                    </div>
+            <Col md={6}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Nom du village :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    value={villageName}
+                    onChange={(e) => setVillageName(e.target.value)}
+                    placeholder="Nom du village اسم القرية"
+                  />
                 </div>
-                </Col>
+              </div>
+            </Col>
 
-                <Col md={6}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Nom du village :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' value={villageName} onChange={e => setVillageName(e.target.value)} placeholder='Nom du village' />
-                    </div>
+            <Col md={12}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Localisation :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    placeholder="Lien Google Maps رابط خرائط جوجل"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                  />
                 </div>
-                </Col>
+              </div>
+            </Col>
 
-                <Col md={12}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Localisation :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' placeholder='Lien Google Maps ' value={location} onChange={e => setLocation(e.target.value)} />
-                    </div>
+            <Col md={6}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Age :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    placeholder="Age العمر"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                  />
                 </div>
-                </Col>
-
-                <Col md={6}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Age :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' placeholder='Age' value={age} onChange={e => setAge(e.target.value)} />
-                    </div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Sexe :</div> */}
+                <div className="form-tab-content">
+                  <select value={sex} onChange={(e) => setSex(e.target.value)}>
+                    <option value="male">Homme/Garçon</option>
+                    <option value="female">Femme/fille</option>
+                    <option value="unknown">Non identifié(e)</option>
+                  </select>
                 </div>
-                </Col>
-                <Col md={6}>
-
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Sexe :</div> */}
-                    <div className='form-tab-content'>
-                    <select value={sex} onChange={e => setSex(e.target.value)}>
-                            <option value="male">Homme/Garçon</option>
-                            <option value="female">Femme/fille</option>
-                            <option value="unknown">Non identifié(e)</option>
-                        </select>
-                    </div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Téléphone (obligatoire) :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    placeholder="Téléphone (obligatoire) الهاتف (مطلوب)"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
                 </div>
-                </Col>
-                <Col md={6}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Téléphone (obligatoire) :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' placeholder='Téléphone (obligatoire)' value={phone} onChange={e => setPhone(e.target.value)} />
-                    </div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Whatsapp :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    placeholder="Whatsapp واتساب"
+                    value={whatsapp}
+                    onChange={(e) => setWhatsapp(e.target.value)}
+                  />
                 </div>
-                </Col>
-                <Col md={6}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Whatsapp :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' placeholder='Whatsapp' value={whatsapp} onChange={e => setWhatsapp(e.target.value)} />
-                    </div>
-                </div>
-                </Col>
-                <Col md={6} xs={6}>
-                <div className='form-tab'>
-                    <a href='/'><div className='btn btn-success retour'> {'< Retour'}</div></a>
-                </div>
-                </Col>
-                <Col md={6} xs={6}>
-                <div className='form-tab'>
-                    <button type='submit' className='btn btn-default form-tab-submit' onClick={handleSubmit}>Soumettre</button>
-                </div>
-                </Col>
-
-            </Row>
-              
-                
-               
-              
-              
-             
-
-               
-                
-            </form>
-            </Container>
-        </>
-    )
-}
+              </div>
+            </Col>
+            <Col md={6} xs={6}>
+              <div className="form-tab">
+                <a href="/">
+                  <div className="btn btn-success retour"> {'< Retour'}</div>
+                </a>
+              </div>
+            </Col>
+            <Col md={6} xs={6}>
+              <div className="form-tab">
+                <button
+                  type="submit"
+                  className="btn btn-default form-tab-submit"
+                  onClick={handleSubmit}
+                >
+                  Soumettre
+                </button>
+              </div>
+            </Col>
+          </Row>
+        </form>
+      </Container>
+    </>
+  );
+};
 
 export default AddMissingPerson;
--- src/components/AddVillage.js
+++ src/components/AddVillage.js
@@ -7,106 +7,160 @@ import { directus } from '../services/directus';
 import { createItem } from '@directus/sdk';
 
 const AddVillage = () => {
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [info, setInfo] = useState('');
+  const [phone, setPhone] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
 
-    const [name, setName] = useState('');
-    const [location, setLocation] = useState('');
-    const [info, setInfo] = useState('');
-    const [phone, setPhone] = useState('');
-    const [whatsapp, setWhatsapp] = useState('');
+  const cleanForm = () => {
+    setName('');
+    setLocation('');
+    setInfo('');
+    setPhone('');
+    setWhatsapp('');
+  };
 
-    const cleanForm = () => {
-        setName('');
-        setLocation('');
-        setInfo('');
-        setPhone('');
-        setWhatsapp('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (phone + whatsapp === '') {
+      window.notifyRed('Besoin de spécifier numéro tel ou whatsapp');
+    } else {
+      try {
+        const village = await directus.request(
+          createItem('villages', {
+            name: name,
+            googlemaplink: location,
+            needs: info,
+            phone: phone,
+            whatsapp: whatsapp,
+          })
+        );
+        window.notify('Village ajouté avec succès.');
+        cleanForm();
+      } catch (error) {
+        window.notifyRed("Erreur lors de l'ajout du village.");
+      }
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (phone + whatsapp === "") {
-            window.notifyRed('Besoin de spécifier numéro tel ou whatsapp')
-        }
-        else {
-            try {
-                const village = await directus.request(createItem('villages', {
-                    name: name,
-                    googlemaplink: location,
-                    needs: info,
-                    phone: phone,
-                    whatsapp: whatsapp
-                }));
-                window.notify('Village ajouté avec succès.');
-                cleanForm();
-            } catch (error) {
-                window.notifyRed('Erreur lors de l\'ajout du village.');
-            }
-        }
-    }
-
-    return(<>
-          <Container>
-            <form className="add-village mt-40">
-            <Row>
-                <h1 className='form-tab-header'>Déclarer un village en besoin</h1>
-                <Col md={12}>
-                
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Nom du village :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' value={name} onChange={e => setName(e.target.value)} placeholder='Nom du village' />
-                    </div>
+  return (
+    <>
+      <Container>
+        <form className="add-village mt-40">
+          <Row>
+            <h1 className="form-tab-header">Déclarer un village en besoin</h1>
+            <div className="d-flex justify-content-center text-center mb-4">
+              <article>
+                <p className="d-inline">
+                  Comment ça marche? Aller sur google map, poser un pin 📍,
+                  cliquer sur partager et copié le lien. Coller le lien dans
+                  "lien google map". Les posts sans numéro de contacts (numéro
+                  de télephone ou what's app) ne seront pas
+                </p>
+                <br />
+                <p className="d-inline font-cairo">
+                  كيف تعمل؟ اذهب إلى خريطة جوجل، ضع دبوس📍، ثم اضغط على مشاركة
+                  وانسخ الرابط. الصق الرابط في "رابط خريطة جوجل". لن يتم
+                  الموافقة على المنشورات التي لا تحتوي على رقم اتصال (رقم الهاتف
+                  او الواتساب)
+                </p>
+              </article>
+            </div>
+            <Col md={12}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Nom du village :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Nom du village اسم القرية"
+                  />
                 </div>
-                </Col>
-                <Col md={12}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Localisation :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' placeholder='Lien Google Maps ' value={location} onChange={e => setLocation(e.target.value)} />
-                    </div>
+              </div>
+            </Col>
+            <Col md={12}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Localisation :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    placeholder="Lien Google Maps رابط خرائط جوجل"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                  />
                 </div>
-                </Col>
-                <Col md={12}>
-                <div className='form-tab'>
-                    <div className='form-tab-title'>Besoins et informations : (facultatif)</div>
-                    <div className='form-tab-content'>
-                        <textarea className='form-tab-input' placeholder='Informations complémentaires' value={info} onChange={e => setInfo(e.target.value)}
-                        ></textarea>
-                    </div>
+              </div>
+            </Col>
+            <Col md={12}>
+              <div className="form-tab">
+                <div className="form-tab-title">
+                  Besoins et informations : (facultatif)
                 </div>
-                </Col>
-                <Col md={6}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Téléphone :</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' placeholder='Numéro de téléphone' value={phone} onChange={e => setPhone(e.target.value)} />
-                    </div>
+                <div className="form-tab-content">
+                  <textarea
+                    className="form-tab-input"
+                    placeholder="Informations complémentaires مزيد من المعلومات"
+                    value={info}
+                    onChange={(e) => setInfo(e.target.value)}
+                  ></textarea>
                 </div>
-                </Col>
-                <Col md={6}>
-                <div className='form-tab'>
-                    {/* <div className='form-tab-title'>Whatsapp : (facultatif)</div> */}
-                    <div className='form-tab-content'>
-                        <input type='text' className='form-tab-input' placeholder='Whatsapp : (facultatif)' value={whatsapp} onChange={e => setWhatsapp(e.target.value)} />
-                    </div>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Téléphone :</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    placeholder="Numéro de téléphone رقم الهاتف"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
                 </div>
-                </Col>
-                <Col md={6} xs={6}>
-                <div className='form-tab'>
-                    <a href='/'><div className='btn btn-success retour'> {'< Retour'}</div></a>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className="form-tab">
+                {/* <div className='form-tab-title'>Whatsapp : (facultatif)</div> */}
+                <div className="form-tab-content">
+                  <input
+                    type="text"
+                    className="form-tab-input"
+                    placeholder="Whatsapp : (facultatif) واتس اب: (اختياري)"
+                    value={whatsapp}
+                    onChange={(e) => setWhatsapp(e.target.value)}
+                  />
                 </div>
-                </Col>
-                <Col md={6} xs={6}>
-                <div className='form-tab'>
-                    <button type='submit' className='btn btn-default form-tab-submit' onClick={handleSubmit}>Soumettre</button>
-                </div>
-                </Col>
-                </Row>
-                
+              </div>
+            </Col>
+            <Col md={6} xs={6}>
+              <div className="form-tab">
+                <a href="/">
+                  <div className="btn btn-success retour"> {'< Retour'}</div>
+                </a>
+              </div>
+            </Col>
+            <Col md={6} xs={6}>
+              <div className="form-tab">
+                <button
+                  type="submit"
+                  className="btn btn-default form-tab-submit"
+                  onClick={handleSubmit}
+                >
+                  Soumettre
+                </button>
+              </div>
+            </Col>
+          </Row>
         </form>
-        </Container>
-        </>
-    )
-}
+      </Container>
+    </>
+  );
+};
 
 export default AddVillage;
